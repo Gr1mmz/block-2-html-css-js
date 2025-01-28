eb816e84b0bc2972f0b195c6a0b99c2f20ba0f24
Mikhail <mikhail.kraevskiy@gmail.com>
2025-01-28 16:43:27 +0800

    disable no-console for line

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,3 @@
 const heading = document.querySelector('.heading');
 
-console.log(heading.innerHTML); // eslint-disable-next-line no-console
+console.log(heading.innerHTML); // eslint-disable-line no-console
